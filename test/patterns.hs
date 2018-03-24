97b853ba7432378f7f413f879eaa812b6a7038a5
Lysxia <lysxia@gmail.com>
2018-03-23 22:40:31 -0400

    Update tests on trees

--- test/patterns.hs
+++ test/patterns.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeApplications #-}
+{-# OPTIONS_GHC -O -fplugin Test.Inspection.Plugin #-}
+{-# OPTIONS_GHC -funfolding-use-threshold=300 #-}
+{-# OPTIONS_GHC -dsuppress-all #-}
 
 import Data.Function ((&))
 import GHC.Generics
@@ -52,13 +56,37 @@ size_manual (Node ns ms) = size_manual ns + (size_manual ms + 1)
 size :: Tree -> Int
 size = gcata $ foldr (+) 1
 
+size1_manual :: Tree -> Int
+size1_manual t = go t id where
+  go End k = k 1
+  go (Leaf _) k = k 1
+  go (Node ns ms) k = go ns (\n -> go ms (\m -> k $! n + m + 1))
+
+size1 :: Tree -> Int
+size1 t = gcata alg t id where
+  alg = case_
+    & match @"End"  (\() k -> k 1)
+    & match @"Leaf" (\_  k -> k 1)
+    & match @"Node" (\(ns, ms) k -> ns (\n -> ms (\m -> k $! n + m + 1)))
+
+size1' :: Tree -> Int
+size1' t = gcata alg t id where
+  alg = case_
+    & match_ @"End"  (\k -> k 1)
+    & match_ @"Leaf" (\_  k -> k 1)
+    & match_ @"Node" (\ns ms k -> ns (\n -> ms (\m -> k $! n + m + 1)))
+
 fib :: (Int, Int) -> [Int]
-fib = gana $ \(a0, a1) ->
+fib = gana $ \(!a0, !a1) ->
   let a2 = a0 + a1 in con @":" (a0, (a1, a2))
 
 fib' :: (Int, Int) -> [Int]
-fib' = gana $ \(a0, a1) ->
+fib' = gana $ \(!a0, !a1) ->
   let a2 = a0 + a1 in con_ @":" a0 (a1, a2)
+
+fib_manual :: (Int, Int) -> [Int]
+fib_manual (!a0, !a1) = a0 : fib_manual (a1, a2) where
+  a2 = a0 + a1
 
 fibTree :: Int -> Tree
 fibTree = gana $ \n ->
@@ -72,14 +100,20 @@ fibTree' = gana $ \n ->
   else if n == 1 then con_ @"Leaf" 1
   else                con_ @"Node" (n - 1) (n - 2)
 
+fibTree_manual :: Int -> Tree
+fibTree_manual = go where
+  go 0 = End
+  go 1 = Leaf 1
+  go n = Node (go (n - 1)) (go (n - 2))
+
 main :: IO ()
 main = do
   let t = Node (Node (Leaf 0) (Leaf 1)) (Leaf 2)
   assertEq (toList  t) [0, 1, 2]
   assertEq (toList' t) [0, 1, 2]
   assertEq (size t) 5
-  -- assertEq (take 6 (fib  (0, 1))) [0, 1, 1, 2, 3, 5]
-  -- assertEq (take 6 (fib' (0, 1))) [0, 1, 1, 2, 3, 5]
+  assertEq (take 6 (fib  (0, 1))) [0, 1, 1, 2, 3, 5]
+  assertEq (take 6 (fib' (0, 1))) [0, 1, 1, 2, 3, 5]
   let sumTree = sum . toList
   assertEq (sumTree (fibTree  5)) 5
   assertEq (sumTree (fibTree' 5)) 5
@@ -91,7 +125,13 @@ assertEq a b = do
   else
     fail $ show a ++ " /= " ++ show b
 
-inspect $ 'toList  ==- 'toList_manual
-inspect $ 'toList' ==- 'toList_manual
-inspect $ 'size    ==- 'size_manual
-inspect $ 'toList1 ==- 'toList1_manual
+inspect $ 'toList   ==- 'toList_manual
+inspect $ 'toList'  ==- 'toList_manual
+inspect $ 'size     ==- 'size_manual
+inspect $ 'size1    ==- 'size1_manual
+inspect $ 'size1'   ==- 'size1_manual
+inspect $ 'toList1  ==- 'toList1_manual
+inspect $ 'fib      ==- 'fib_manual
+inspect $ 'fib'     ==- 'fib_manual
+inspect $ 'fibTree  ==- 'fibTree_manual
+inspect $ 'fibTree' ==- 'fibTree_manual
